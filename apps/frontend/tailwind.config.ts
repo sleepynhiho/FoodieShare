3396bc1910f0108dc46ebf4fe4b3c122ba195036
NgHuyen <nguyenngocnhuhuyen1307@gmail.com>
2025-09-06 06:22:35 +0700

    chore: config shadcn/ui

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -14,11 +14,45 @@ export default {
           default: "#fffefd", // body background
           card: "#ffffff",    // card / container
           button: "#f6f6f6",  // button background
+          icon: "#fff7ecff"     // icon background
         },
         text: {
           primary: "#ffa319", // orange
           default: "#111111", // main text
           muted: "#4B5563",   // gray text
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
       borderRadius: {
@@ -32,6 +66,9 @@ export default {
       fontFamily: {
         sans: ["Inter", "ui-sans-serif", "system-ui"],
       },
+      screens: {
+        "xs": "360px",
+      }
     },
   },
   plugins: [],
