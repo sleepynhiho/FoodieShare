d6e1889d80bdb99358ff134e8f49ea51a8b26901
quynhchi <doquynhchi1311@gmail.com>
2025-09-06 22:41:19 +0700

    create Add Recipe Page UI and connect to mock data

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -14,12 +14,17 @@ export default {
           default: "#fffefd", // body background
           card: "#ffffff",    // card / container
           button: "#f6f6f6",  // button background
+          secondary: "#ffa319"
         },
         text: {
           primary: "#ffa319", // orange
           default: "#111111", // main text
           muted: "#4B5563",   // gray text
         },
+        border: {
+          default: "#E5E7EB", // gray-200
+          primary: "#ffa319", // orange
+        }
       },
       borderRadius: {
         md: "0.5rem",
