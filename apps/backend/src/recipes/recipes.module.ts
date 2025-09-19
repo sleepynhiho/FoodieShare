690dfb9fdfb9068f44d960402b4a5b41c8c71d29
sleepynhiho <nhiho.wkac@gmail.com>
2025-09-19 18:03:22 +0700

    fix: fix guard logic

--- apps/backend/src/recipes/recipes.module.ts
+++ apps/backend/src/recipes/recipes.module.ts
@@ -3,9 +3,10 @@ import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
 import { PrismaModule } from 'src/common/prisma/prisma.module';
 import { SupabaseModule } from 'src/common/supabase/supabase.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, SupabaseModule],
+  imports: [PrismaModule, SupabaseModule, AuthModule],
   controllers: [RecipesController],
   providers: [RecipesService],
   exports: [RecipesService],
